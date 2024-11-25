9e3c67ad8eeffe1f5e69010a4902e87998e6b74f
MarcoC567 <cama1025@h-ka.de>
2024-11-25 18:03:46 +0100

    Config, Navbar und Homepage

---
+++ src/app/pages/Navbar.tsx
@@ -0,0 +1,44 @@
+'use client';
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+
+export const Navbar = () => {
+  const [activeLink, setActiveLink] = useState("#");
+
+  const handleLinkClick = (tabName: string) => setActiveLink(tabName);
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand"href="#" passHref>
+          <Image
+            src="/book.svg"
+            alt="Book Icon"
+            width="30"
+            height="30"
+            className="d-inline-block align-text-top mx-4"
+          />
+          Bookstore
+        </Link>
+        <div className="d-flex w-100 align-items-center">
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+              <a  className={`nav-link ${activeLink === "Suchen" ? "active" : ""}`} 
+              aria-current="page" href="#" onClick={() => handleLinkClick("Suchen")}>
+                Suchen
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className={`nav-link ${activeLink === "Hinzufügen" ? "active" : ""}`} href="#" onClick={() => handleLinkClick("Hinzufügen")}>
+                Hinzufügen
+              </a>
+            </li>
+          </ul>
+          <Button className="ms-auto mx-4" href="Login">Login</Button>
+        </div>
+      </div>
+    </nav>
+  );
+};
